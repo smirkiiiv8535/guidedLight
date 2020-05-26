dc3ad23d09719480938f1251c6e2064585a3d6c2
yuchen <smirkiiiv@gmail.com>
2020-05-26 17:38:46 +0800

    Initial Commit

---
+++ guidedLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guidedLight
+//
+//  Created by 林祐辰 on 2020/5/26.
+//  Copyright © 2020 smirkiiiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
